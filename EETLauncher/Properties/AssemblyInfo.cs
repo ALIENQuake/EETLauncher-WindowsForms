a2298aa5b54b0abae3b0ced29644b9f7175e5f5d
ALIENQuake <alienquake@hotmail.com>
2018-06-29 22:34:37 +0200

    Copyright © alienquake@hotmail.com

--- EETLauncher/Properties/AssemblyInfo.cs
+++ EETLauncher/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("EETLauncher")]
-[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyCopyright( "Copyright © alienquake@hotmail.com" )]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
